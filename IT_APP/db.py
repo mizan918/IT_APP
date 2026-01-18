dd7c0c523f29496f136e59efc7bfbde15fa2a8e2
mizan918 <52394124+mizan918@users.noreply.github.com>
2026-01-18 17:29:50 +0600

    Add files via upload

--- IT_APP/db.py
+++ IT_APP/db.py
@@ -1,18 +1,21 @@
-import oracledb
+import os
 
-oracledb.init_oracle_client(lib_dir=r"D:\App\Zobair\product\11.2.0\client_1")
-
-# Adjust username, password, and service name
-dsn = "192.168.21.14:1521/testababil"   # If service name is different, replace ORCL
+DB_MODE = os.getenv("DB_MODE", "LOCAL")  # LOCAL or CLOUD
 
 def get_connection():
-    conn = oracledb.connect(
-        user="mp_dec25",           # <-- replace with your Oracle username
-        password="a",   # <-- replace with your Oracle password
-        dsn=dsn
-    )
-    return conn
-    
-    
- 
- 
+    if DB_MODE == "LOCAL":
+        import oracledb
+
+        oracledb.init_oracle_client(
+            lib_dir=r"D:\App\Zobair\product\11.2.0\client_1"
+        )
+
+        return oracledb.connect(
+            user="mp_dec25",
+            password="a",
+            dsn="192.168.21.14:1521/testababil"
+        )
+
+    else:
+        # Cloud / Render mode (NO Oracle)
+        return None
